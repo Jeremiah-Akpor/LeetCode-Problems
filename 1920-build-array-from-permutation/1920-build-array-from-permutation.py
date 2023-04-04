e6093fde93ae08e717a62c55355a01eca3e8c6ab
Jeremiah_Akpor <112021714+Jeremiah-Akpor@users.noreply.github.com>
2023-04-04 19:30:51 +0200

    Time: 114 ms (96.16%), Space: 14.2 MB (36.19%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.py
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.py
@@ -1,21 +1,14 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
-        # 1. get the length of the array
-        n = len(nums)
-
-        """
-            The code creates a new list ans with the same length as the input list nums,
-            where each element in the new list is initialized to zero.
-            For example, if nums is [2, 5, 3, 7], the code ans = [0] * len(nums) will 
-            create a new list ans with the elements [0, 0, 0, 0].
-        """
-        ans = [0] * n
+       
+        # 1. create an empty array 
+        ans = [] 
 
         # 3. create for loop then iterates through the nums array
-        # and inserts the element of the nums array into the ans 
+        # and append the element of the nums array into the ans 
         # array 
-        for i in range(n):
-            ans[i] = nums[nums[i]]
+        for i in range(len(nums)):
+            ans.append(nums[nums[i]])
         
         return ans
         
