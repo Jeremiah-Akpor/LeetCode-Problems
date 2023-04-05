27b6bde665f69dd20db7241cbc015225c8698495
Jeremiah_Akpor <112021714+Jeremiah-Akpor@users.noreply.github.com>
2023-04-05 20:38:37 +0200

    Time: 73 ms (77.41%), Space: 14.1 MB (92.04%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.py
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.py
@@ -1,9 +1,29 @@
 class Solution:
     def leftRigthDifference(self, nums: List[int]) -> List[int]:
+        
+        """
+        The first for loop Returns:
+            A list of integers where the i-th element is the sum of the first i+1
+            elements in the input list.
+
+        Example:
+            >>> compute_running_sum([1, 2, 3, 4])
+            [1, 3, 6, 10]
+        
+        The second for loop calculates the absolute difference between the sum of the first i-1 elements
+        and the sum of the last n-i+1 elements, for i in the range 1 to n-1 inclusive.
+        """
+        x = 0
+        temp = []
+        for num in nums:
+            x += num
+            temp.append(x)
+        
         ans = []
-        for i in range(len(nums)):
-            left = sum(nums[:i])
-            right = sum(nums[i+1:])
+        last = temp[-1]
+        ans.append(last - nums[0])
+        for i in range(1, len(nums)):
+            right = last - temp[i]
+            left = temp[i-1]
             ans.append(abs(left - right))
         return ans
-        
