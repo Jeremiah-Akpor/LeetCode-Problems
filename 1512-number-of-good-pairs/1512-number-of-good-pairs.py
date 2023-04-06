2d6fe8c829897f0d14d5491945bd34eec6f45cf5
Jeremiah_Akpor <112021714+Jeremiah-Akpor@users.noreply.github.com>
2023-04-06 17:42:16 +0200

    Time: 36 ms (54.23%), Space: 13.8 MB (92.86%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -1,14 +1,31 @@
 class Solution:
     def numIdenticalPairs(self, nums: list[int]) -> int:
-            hashtable = {}
-            for num in nums:
-                if num not in hashtable:
-                    hashtable[num] = 1
-                else:
-                    hashtable[num] += 1
+        """
+        Given an integer array nums, return the number of good pairs.
+        A pair (i,j) is called good if nums[i] == nums[j] and i < j.
 
-            ans = 0
-            for value in hashtable.values():
-                if value > 1 :
-                    ans += (value *(value - 1)) / 2
-            return int(ans)
+        Args:
+            nums (list[int]): An array of integers.
+
+        Returns:
+            int: The number of good pairs.
+
+        Examples:
+            >>> numIdenticalPairs([1, 2, 3, 1, 1, 3])
+            4
+            >>> numIdenticalPairs([1, 1, 1, 1])
+            6
+        """
+        hashtable = {}
+        for num in nums:
+            if num not in hashtable:
+                hashtable[num] = 1
+            else:
+                hashtable[num] += 1
+
+        ans = 0
+        for value in hashtable.values():
+            if value > 1 :
+                ans += (value *(value - 1)) / 2
+        return int(ans)
+
