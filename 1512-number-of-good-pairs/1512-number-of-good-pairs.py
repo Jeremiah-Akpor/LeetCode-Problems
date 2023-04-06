d9c79f1ed6cd93c4d468231ac2f510a00636734a
Jeremiah_Akpor <112021714+Jeremiah-Akpor@users.noreply.github.com>
2023-04-06 17:37:08 +0200

    Time: 34 ms (68.93%), Space: 13.8 MB (92.86%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numIdenticalPairs(self, nums: list[int]) -> int:
+            hashtable = {}
+            for num in nums:
+                if num not in hashtable:
+                    hashtable[num] = 1
+                else:
+                    hashtable[num] += 1
+
+            ans = 0
+            for value in hashtable.values():
+                if value > 1 :
+                    ans += (value *(value - 1)) / 2
+            return int(ans)
