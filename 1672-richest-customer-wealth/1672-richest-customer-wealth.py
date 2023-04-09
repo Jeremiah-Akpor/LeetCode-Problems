fea12512d53da46fa8f3c2cc33047c698de00c6e
Jeremiah_Akpor <112021714+Jeremiah-Akpor@users.noreply.github.com>
2023-04-09 08:07:25 +0200

    Time: 48 ms (93.61%), Space: 13.9 MB (19.85%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.py
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.py
@@ -1,13 +1,7 @@
 class Solution:
     def maximumWealth(self, accounts: list[list[int]]) -> int:
-        m = len(accounts) -1
-        a = m // 2 
-        b = a +1
         x = 0
-        for i in range(0,b):
-            if not (b > m):
-                x = max (x , sum(accounts[b]))
-                b += 1
-            x = max(x , sum(accounts[i]))
+        for account in accounts:
+            x = max(x , sum(account))
         return x
         
