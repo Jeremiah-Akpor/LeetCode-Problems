a79ab784ec4b18003815f14a4d35beaad60d79fb
Jeremiah_Akpor <112021714+Jeremiah-Akpor@users.noreply.github.com>
2023-04-28 11:26:40 +0200

    Update 0121-best-time-to-buy-and-sell-stock.py

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,19 +1,5 @@
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
-        """
-            You are given an array prices where prices[i]
-            is the price of a given stock on the ith day.
-            You want to maximize your profit by choosing a
-            single day to buy one stock and choosing a
-            different day in the future to sell that stock.
-        Args:
-            prices (list[int]): natural number
-
-        Returns:
-            int: Return the maximum profit you can
-            achieve from this transaction. If you cannot
-            achieve any profit, return 0
-        """
         c_min = prices[0]
         c_profit = 0
         profit = 0
